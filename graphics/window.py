fe1a42e5bf9f6ae31045b631439cca3f3299aaff
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-01-08 12:12:01 -0500

    draw lines

--- graphics/window.py
+++ graphics/window.py
@@ -1,4 +1,8 @@
 from tkinter import Tk, BOTH, Canvas
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from drawing import Line
 
 class Window():
     def __init__(self, w, h) -> None:
@@ -15,6 +19,9 @@ class Window():
         # running
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def draw_line(self, line: "Line", fill_color: str):
+        line.draw(self.__canvas, fill_color)
 
     def redraw(self):
         self.__root.update_idletasks()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from graphics import *
+from graphics.drawing import Line, Point
 
 def main():
+    test_line = Line(Point(0,20), Point(500,200))
+
     win = window.Window(800, 600)
+    win.draw_line(test_line, "white")
     win.wait_for_close()
 
 if __name__ == "__main__":
